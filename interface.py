44d3c0a41f1e37c658928082674acda52754ee20
DMLen <dmlennox03@gmail.com>
2024-05-21 17:11:47 +1000

    initial file creation (unfinished)

---
+++ interface.py
@@ -0,0 +1,19 @@
+class DeviceManager:
+    def __init__(self):
+
+    #create device
+    def createDevice(self, name, consumption, priority, state):
+        #add api call
+        #return results of api
+        
+    #fetch devices
+    def fetchDevices():
+        #return error 1
+
+        #return dictionary of all devices
+
+    #fetch device (single)
+    
+    #delete device
+    #update device
+
