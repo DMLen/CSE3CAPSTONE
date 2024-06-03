dff434ca4fea54c17a5d0ed67be50de23f1d20ae
DMLen <dmlennox03@gmail.com>
2024-06-03 19:23:23 +1000

    updated plugtest file

--- plugtest.py
+++ plugtest.py
@@ -1,12 +1,19 @@
 from priority import Device
 from PyP100 import PyP110
+from time import sleep
 
 ip1 = "192.168.0.225"
+ip2 = "192.168.0.226"
 emailaddr = "21240797@students.latrobe.edu.au"
 passw = "mbw28bag"
 
-testplug1 = Device(ip1, emailaddr, passw, "Plug 1", 1)
+testplug1 = Device(ip1, emailaddr, passw, "Lamp", 1)
+testplug2 = Device(ip2, emailaddr, passw, "Heater", 1)
 #testplug1 = PyP110.P110(ip1, emailaddr, passw)
 
 testplug1.turnOn()
-testplug1.turnOffWithDelay(5)
+testplug2.turnOn()
+sleep(5)
+print(f"{testplug1.name} info: {testplug1.returnConsumption()} watts!\n{testplug2.name} info: {testplug2.returnConsumption()} watts!")
+testplug1.turnOff()
+testplug2.turnOff()
